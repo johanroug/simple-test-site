d96825be2074a7839261cdede4543aa26a6a2dd9
Johan roug <johanroug@gmail.com>
2013-02-09 23:28:12 +0100

    add

---
+++ resources/js/script.js
@@ -0,0 +1,22 @@
+(function($){ 
+
+	//Document ready
+	$(function(){ 
+		
+	});
+
+	//Window resize
+	$(window).resize(function () {
+
+	});
+  
+	//Window load
+	$(window).load(function (){
+	
+	});	
+
+
+
+	
+
+})( jQuery );
